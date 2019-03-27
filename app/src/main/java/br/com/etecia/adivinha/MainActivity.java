d6388b2be884c0e70e5337b393772bafdfc3ca4d
gabrielmascarenhas <gabriel.mascarenhas@etec.sp.gov.br>
2019-03-26 21:28:33 -0300

    upload project

---
+++ app/src/main/java/br/com/etecia/adivinha/MainActivity.java
@@ -0,0 +1,42 @@
+package br.com.etecia.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private Button botaoJogar;
+    private TextView textoResultado;
+    private Button botaoLimpar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.botaoJogar);
+        textoResultado = findViewById(R.id.textoResultado);
+        botaoLimpar = findViewById(R.id.botaoLimpar);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random random = new Random();
+               int resultado = random.nextInt(10);
+                textoResultado.setText("O resultado é: "+resultado);
+            }
+
+        });
+        botaoLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textoResultado.setText("");
+            }
+        });
+    }
+}
